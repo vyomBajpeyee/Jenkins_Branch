f46ce56cb6738ba19dfd42f1f1043c7e72e557a7
vyomBajpeyee <vyombajpeyee@gmail.com>
2018-08-14 12:47:27 +0530

    changed location to bug

--- hello.java
+++ hello.java
@@ -1,6 +1,6 @@
 public class hello {
     public static void main(String[] args) {
       System.out.println("Hello, World");
-	System.out.println("I am in Master Branch");
+	System.out.println("I am in Bug Branch");
     }
 }
